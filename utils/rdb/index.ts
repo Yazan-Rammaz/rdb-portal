9e64f003abe1e980fc5b18b3547c0fe05811342c
Yazan Ad <yazan.adnof@ramaaz.com>
2026-02-17 14:36:18 +0100

    new  structure

--- utils/rdb/index.ts
+++ utils/rdb/index.ts
@@ -1,25 +1,12 @@
-// src/actions/index.ts
-import * as actions from "./actions";
+import { rdbActionBridge } from '@/utils/rdb/rdb-server';
 
-export const serverActions = {
-  banking: {
-    getCurrencies: actions.getCurrencies,
-    GetBanks: actions.GetBanks,
-    CreateBankDeposit: actions.CreateBankDeposit,
-    GetBankDeposits: actions.GetBankDeposits,
-    CalculateFees: actions.CalculateFees,
-  },
-  media: {
-    UploadMedia: actions.UploadMedia,
-  },
-  transactions: {
-    GetWalletBalance: actions.GetWalletBalance,
-    GetJournalEntries: actions.GetJournalEntries,
-    GetTransactions: actions.GetTransactions,
-    CheckoutOrder: actions.CheckoutOrder,
-  },
-  wallets: {
-    checkWallet: actions.checkWallet,
-    createWallet: actions.createWallet,
-  }
-};
+export const serverActions = new Proxy({} as any, {
+    get(_, namespace: string) {
+        return new Proxy({}, {
+            get(_, action: string) {
+                // Return an async function that calls the bridge
+                return (args: any) => rdbActionBridge(namespace, action, args);
+            }
+        });
+    }
+});
