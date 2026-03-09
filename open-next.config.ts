47a1b924e25d2b0977ab45dcc16fa86481febdbd
Yazan Ad <yazan.adnof@ramaaz.com>
2026-03-09 10:51:45 +0100

    deploy cloudflare

---
+++ open-next.config.ts
@@ -0,0 +1,4 @@
+import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+
+// This file is used to configure the Cloudflare deployment for your OpenNext.js application
+export default defineCloudflareConfig();
